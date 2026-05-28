05932d3445c06c12281a10a1e64e7f7051e4818e
Muru24 <162981848+Muru24@users.noreply.github.com>
2026-05-28 11:25:30 +0900

    Reward작업중

--- SlayTheChampions/Source/SlayTheChampions/Public/Map/RewardSystem.h
+++ SlayTheChampions/Source/SlayTheChampions/Public/Map/RewardSystem.h
@@ -30,24 +30,6 @@ private:
 	UPROPERTY()
 	FAreaInfo ClearedAreaInfo;
 
-	/* Area 타입별 클리어 보상 골드 */
-	UPROPERTY()
-	int32 NormalAreaClearGold = 10;
-
-	UPROPERTY()
-	int32 EliteAreaClearGold = 25;
-
-	UPROPERTY()
-	int32 BossAreaClearGold = 50;
-
-	/* 현재 보유 골드 */
-	UPROPERTY()
-	int32 CurrentGold = 0;
-
-	/* 현재 클리어 보상으로 받을 대기 골드 */
-	UPROPERTY()
-	int32 PendingGoldReward = 0;
-
 public:
 	void Initialize(URunSystem* InRunSystem);
 	/* Area 클리어 보상 창 처리 */
@@ -62,27 +44,13 @@ public:
 	UFUNCTION(BlueprintCallable)
 	void ReturnToMapAfterReward();
 
-	/* Area 타입별 클리어 보상 골드 설정 */
-	UFUNCTION(BlueprintCallable)
-	void SetAreaClearGoldRewardValues(int32 InNormalAreaClearGold, int32 InEliteAreaClearGold, int32 InBossAreaClearGold);
-
 	/* Area 클리어 보상 UI 데이터 설정 */
 	UFUNCTION(BlueprintCallable)
 	void SetAreaClearUiData(UAreaClearUiData* InAreaClearUiData);
 
-	/* 현재 보유 골드 반환 */
-	UFUNCTION(BlueprintPure)
-	int32 GetCurrentGold() const { return CurrentGold; }
-
-	/* 현재 클리어 보상으로 받을 대기 골드 반환 */
-	UFUNCTION(BlueprintPure)
-	int32 GetPendingGoldReward() const { return PendingGoldReward; }
-
 private:
 	/* Area 클리어 기본 데이터 로드 */
 	bool LoadDefaultAreaClearUiData();
-	/* Area 클리어 보상 골드 계산 */
-	int32 GetAreaClearGoldReward(const FAreaInfo& RoomInfo) const;
 	/* Area 클리어 보상 UI 열기 */
 	void OpenAreaClearWidget();
 };
