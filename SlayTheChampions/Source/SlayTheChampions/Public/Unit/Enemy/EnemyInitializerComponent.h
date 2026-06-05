5f844af954e02a8165100606d0c539f6073b4938
Seyongski <jsy010618@gmail.com>
2026-06-05 10:26:04 +0900

    적 데이터에셋 설계
    .

---
+++ SlayTheChampions/Source/SlayTheChampions/Public/Unit/Enemy/EnemyInitializerComponent.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Unit/Enemy/EnemyDefinitionData.h"
+#include "EnemyInitializerComponent.generated.h"
+
+class StatComponent;
+class NPCBrainComponent;
+class StatusEffectComponent;
+
+UCLASS( ClassGroup=(Unit), meta=(BlueprintSpawnableComponent) )
+class SLAYTHECHAMPIONS_API UEnemyInitializerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UEnemyInitializerComponent();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy")
+	TObjectPtr<UEnemyDefinitionData> Definition;
+
+	// 스폰 직후 1회 호출 → 유닛 구성
+	UFUNCTION(BlueprintCallable, Category = "Enemy")
+	void InitializeFromDefinition(UEnemyDefinitionData* Def);
+	
+		
+};
